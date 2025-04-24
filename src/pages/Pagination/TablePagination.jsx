46bb2467a9ce88de5cb3b986fae04a31b73c6674
Dominic-SR <dominicseduraja3@gmail.com>
2025-04-24 15:26:45 +0530

    pagination functionality completed

--- src/pages/Pagination/TablePagination.jsx
+++ src/pages/Pagination/TablePagination.jsx
@@ -3,7 +3,7 @@ import React, { useState,useEffect } from 'react'
 const TablePagination = () => {
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(true);
-  const [currentPage, setCurrentPage] = useState(2);
+  const [currentPage, setCurrentPage] = useState(1);
   const [postPerPage, setPostPerPage] = useState(10)
 
   useEffect(() => {
@@ -26,6 +26,10 @@ const TablePagination = () => {
 
   const currentPost =  data.slice(indexOfFirstPost, indexOfLastPost);
 
+  const totalPages = Math.ceil(data.length / postPerPage)
+  const paginate = (page) =>{setCurrentPage(page)}
+
+
 if(loading)return <p>Loading....</p>
   return (
     <>
@@ -37,6 +41,20 @@ if(loading)return <p>Loading....</p>
           </li>
         ))}
       </ul>
+
+
+
+      <div className='pagination'>
+          <button onClick={()=>paginate(1)}>First</button>
+          <button disabled={currentPage === 1} onClick={()=>paginate(currentPage - 1)}>prev</button>
+          {
+            new Array(totalPages).fill(0).map((_,index)=>{
+              return <button className={currentPage === index + 1 ? "active":""} onClick={()=>{paginate(index + 1)}} key={index + 1}>{index+1}</button>
+            })
+          }
+          <button disabled={currentPage === totalPages} onClick={()=>paginate(currentPage + 1)}>Next</button>
+          <button onClick={()=>paginate(totalPages)}>Last</button>
+      </div>
     </>
   )
 }
