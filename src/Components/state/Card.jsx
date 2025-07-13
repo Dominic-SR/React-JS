87affdb20040dd1cd3b7338a3c29bfa99d877470
Dominic-SR <dominicseduraja3@gmail.com>
2025-07-13 23:57:58 +0530

    card functions added

--- src/Components/state/Card.jsx
+++ src/Components/state/Card.jsx
@@ -4,9 +4,13 @@ import { data } from './../../assets/DataSet'
 const Card = () => {
 const [totalNoOfProduct, setTotalNoOfProduct] = useState(data.length)
 const [products, setProducts] = useState(data)
+
+const handleDeleteAll = () =>[
+    
+]
   return (
     <div>
-        <h1>Items in cart:{totl}</h1>
+        <h1>Items in cart:{totalNoOfProduct}</h1>
         {products.map((product)=>{
             return(
                 <div>
