f79af88545b9eb9a2b338c17e1f736875d635a49
Dominic-SR <dominicseduraja3@gmail.com>
2025-07-13 23:55:11 +0530

    use state added

--- src/Components/state/Card.jsx
+++ src/Components/state/Card.jsx
@@ -2,10 +2,11 @@ import React, { useState } from 'react'
 import { data } from './../../assets/DataSet'
 
 const Card = () => {
+const [totalNoOfProduct, setTotalNoOfProduct] = useState(data.length)
 const [products, setProducts] = useState(data)
   return (
     <div>
-        <h1>Items in cart:</h1>
+        <h1>Items in cart:{totl}</h1>
         {products.map((product)=>{
             return(
                 <div>
