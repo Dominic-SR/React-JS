32acd80bae7348b9304f229076a728c04783c4a2
Dominic-SR <dominicseduraja3@gmail.com>
2025-07-14 23:28:56 +0530

    single delete and overall delete function

--- src/Components/state/Card.jsx
+++ src/Components/state/Card.jsx
@@ -5,23 +5,30 @@ const Card = () => {
 const [totalNoOfProduct, setTotalNoOfProduct] = useState(data.length)
 const [products, setProducts] = useState(data)
 
-const handleDeleteAll = () =>[
-    
-]
+const handleDelete = (id) =>{
+    const updateProduct = products.filter((x)=>x.id != id);
+    setProducts(updateProduct);
+    setTotalNoOfProduct(updateProduct.length)
+}
+
+const handleDeleteAll = () =>{
+    setProducts([])
+    setTotalNoOfProduct(0)
+}
   return (
     <div>
         <h1>Items in cart:{totalNoOfProduct}</h1>
         {products.map((product)=>{
             return(
-                <div>
+                <div key={product.id}>
                     <p>
                         {product.name}, Rs{product.Price}
                     </p>
-                    <button>Delete</button>
+                    <button onClick={()=>handleDelete(product.id)}>Delete</button>
                 </div>
             )
         })}
-        <button>Delete All</button>
+        <button onClick={handleDeleteAll}>Delete All</button>
     </div>
   )
 }
