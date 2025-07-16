8041c3926356899ab782afc2364d726bfaf05c09
Dominic-SR <dominicseduraja3@gmail.com>
2025-07-16 23:43:03 +0530

    call components

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import Card from './Components/state/Card'
 import Counter from './Components/state/Counter'
 import UseEffectExample from './Components/effect/useEffectExample'
+import GithubUsers from './Components/effect/GithubUsers'
+import Timer from "./Components/effect/Timer"
+import ButtonClickCount from "./Components/callback/ButtonClickCounter"
+
 
 function App() {
 
   return (
-    <UseEffectExample />
-  //  <Counter />
+    <ButtonClickCount />
+  // <Timer />
+  // <GithubUsers />
+  // <UseEffectExample />
   // <Card />
+  //  <Counter />
   )
 }
 
--- src/Components/effect/UseEffectExample.jsx
+++ src/Components/effect/UseEffectExample.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import React,{useState, useEffect} from 'react'
 
 const UseEffectExample = () => {
+  const [count1, setCount1] = useState(0);
+  const [count2, setCount2] = useState(0);
+
+  useEffect(()=>{
+    console.log("component 1");
+  },[count1])
+
+  useEffect(()=>{
+    console.log("component 2");
+  },[count2])
+  
+  const handleIncrement1 = () =>{
+    setCount1((prev)=>(prev + 1))
+  }
+
+  const handleIncrement2 = () =>{
+    setCount2((prev)=>(prev + 1))
+  }
+
   return (
-    <div>one</div>
+    <div className='container mt-5'>
+        <h2 className='fw-bold'>Counter {count1}</h2>
+        <button className='btn btn-primary' onClick={handleIncrement1}>Button 1</button>
+        <h2 className='fw-bold'>Counter {count2}</h2>
+        <button className='btn btn-primary' onClick={handleIncrement2}>Button 2</button>
+    </div>
   )
 }
 
