d57472c2e8b3eb57288c0832ec0d7449a594a4a0
Dominic-SR <dominicseduraja3@gmail.com>
2025-12-16 23:25:54 +0530

    react changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,38 +4,35 @@ import UseEffectExample from './Components/effect/useEffectExample'
 import GithubUsers from './Components/effect/GithubUsers'
 import Timer from "./Components/effect/Timer"
 import ButtonClickCount from "./Components/callback/ButtonClickCounter"
-import Home from './Components/home/Home'
 import TaskApp from './Components/reducer/TaskApp'
 import OnlyUseReduser from './Components/reducer/OnlyUseReducer'
 import SortingMemo from './Components/memo/sortingmemo'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './Pages/Home/Home'
 
 
 function App() {
 
   return (
-<<<<<<< HEAD
+  <BrowserRouter>
+  <Routes>
+    <Route path="/" element={<Home />} />
+  </Routes>
+  </BrowserRouter>  
   //UseState
   // <Card />
-  <Counter />
-
+  // <Counter />
   // useReducer
   // <OnlyUseReduser /> 
-  
   //useCallback
   // <ButtonClickCount /> 
-=======
   //--UseState--//
   // <Counter />
-   <Card />
-  
+  //  <Card />
   //--UseEffect--//
   //  <Timer />
->>>>>>> d9160246ba3ac391ab35a65ae8f8f843903016e7
-
   // <SortingMemo />
   //  <TaskApp />
-  
-
   // <Home />
   // <Timer />
   // <GithubUsers />
---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+  <ul class="list-group">
+        <li class="list-group-item active">React Concepts</li>
+    <li class="list-group-item">Hooks
+
+       <li class="list-group-item">Dapibus ac facilisis in</li>
+    <li class="list-group-item">Morbi leo risus</li>
+    </li>
+   
+    <li class="list-group-item">Porta ac consectetur ac</li>
+    <li class="list-group-item">Vestibulum at eros</li>
+  </ul>
+  )
+}
+
+export default Home
