bbc08e49fe8998f1509814e4a3036038a78ff73b
Dominic-SR <dominicseduraja3@gmail.com>
2025-12-16 23:36:50 +0530

    menus added

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,17 +2,23 @@ import React from 'react'
 
 const Home = () => {
   return (
-  <ul class="list-group">
-        <li class="list-group-item active">React Concepts</li>
-    <li class="list-group-item">Hooks
+  <ul class="list-group w-50 m-auto">
+  <li class="list-group-item bg-info text-light fw-bold">REACT CONCEPTS</li>
+  <li class="list-group-item">
+    Hooks
+    <ul class="list-group mt-2">
+      <li class="list-group-item ps-4">UseState
+          <ul class="list-group mt-2">
+            <li class="list-group-item ps-4">Counter</li>
+            <li class="list-group-item ps-4">Add to Card</li>
+          </ul>
+      </li>
+      <li class="list-group-item ps-4">UseEffect</li>
+    </ul>
+  </li>
+  <li class="list-group-item">Main Item 3</li>
+</ul>
 
-       <li class="list-group-item">Dapibus ac facilisis in</li>
-    <li class="list-group-item">Morbi leo risus</li>
-    </li>
-   
-    <li class="list-group-item">Porta ac consectetur ac</li>
-    <li class="list-group-item">Vestibulum at eros</li>
-  </ul>
   )
 }
 
