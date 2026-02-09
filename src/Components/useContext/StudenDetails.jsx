26c0d520c6fbfc4fb5a666e2371e4194fcf628d6
Dominic-SR <dominicseduraja3@gmail.com>
2026-02-09 22:35:27 +0530

    use contect and dataset creation

---
+++ src/Components/useContext/StudenDetails.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const StudenDetails = () => {
+  return (
+    <div>StudenDetails</div>
+  )
+}
+
+export default StudenDetails
--- src/assets/DataSet.js
+++ src/assets/DataSet.js
@@ -4,3 +4,11 @@ export const data = [
     {id:3, name:"MacBook pro", Price:88000},
     {id:4, name:"Samsung Galexy 6", Price:25000},
 ]
+
+export const studentDate = {
+    name:"john Doe",
+    age: 16,
+    department: "Information technology",
+    subjects: ["maths","hsitory","English"],
+    contactInformation: {phone:9876543210,address:"1st cross street"}
+}
