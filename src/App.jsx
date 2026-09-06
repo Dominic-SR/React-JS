8e8d0414eafd40428dbcf100be2b7e8e1a0fb97b
Dominic-SR <dominicseduraja3@gmail.com>
2026-09-06 22:30:16 +0530

    use callback changes update

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Card from './Components/useState/Card/Card'
 import Counter from './Components/useState/Counter/Counter'
 import UseEffectCounter from './Components/useEffect/Counter/UseEffectCounter'
 import GithubUsers from './Components/useEffect/GithubUsers/GithubUsers'
-import Timer from "./Components/useEffect/Timer/TimerCleaUpFunction"
+import Timer from "./Components/useEffect/TimerCleanerFunction/TimerCleaUpFunction"
 import MemroizesFunction from './Components/useCallback/MemoizesFunction/MemoizesFunction'
 import TaskApp from './Components/useReducer/TaskManagement/TaskApp'
 import TaskManagement from './Components/useReducer/TaskManagement/TaskManagement'
@@ -41,9 +41,10 @@ function App() {
     <Route path="/useref_loginform" element={<LoginForm />} />
     <Route path="/useref_autofocusinput" element={<AutoFocusInput />} />
 
-
+    {/* useContext */}
     <Route path="/students_details" element={<Students/>} />
 
+    {/* useCallback */}
     <Route path="/usecallback_memroizesfunction" element={<MemroizesFunction/>} />
 
     <Route path="/usetransition_without_transition" element={<WithoutTransition />} />
